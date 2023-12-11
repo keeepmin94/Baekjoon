17bc92651cc14b23c5dcb0ccec895e5bb4596c11
keeepmin94 <wjdwkjh94@gmail.com>
2023-12-11 10:39:44 +0900

    백준-19532

---
+++ bruteforce/19532.js
@@ -0,0 +1,17 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = require("fs").readFileSync(path).toString().trim().split("\n");
+const inputs = input[0].split(" ").map(Number);
+
+function solution(inputs) {
+  const [a, b, c, d, e, f] = inputs;
+
+  for (let x = -999; x <= 999; x++) {
+    for (let y = -999; y <= 999; y++) {
+      if (a * x + b * y === c && d * x + e * y === f) {
+        console.log(`${x} ${y}`);
+      }
+    }
+  }
+}
+
+solution(inputs);
